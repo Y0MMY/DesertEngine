c406fb537869d7547495cdc8d341fa4a8f207d6c
Danya <daniljudoist@inbox.ru>
2025-06-01 20:47:02 +0300

    vulkan resize imgui fix

--- Desert/Desert/Source/Engine/imgui/UI/ImGui.cpp
+++ Desert/Desert/Source/Engine/imgui/UI/ImGui.cpp
@@ -10,32 +10,32 @@
 namespace Desert::ImGui::UI
 {
 
-    static std::unordered_map<std::shared_ptr<Graphic::Image2D>, ImTextureID> g_TextureCache;
-
     void Image( const std::shared_ptr<Graphic::Image2D>& image, const ImVec2& size, const ImVec2& uv0,
-                const ImVec2& uv1, const ImVec4& tint_col, const ImVec4& border_col)
+                const ImVec2& uv1, const ImVec4& tint_col, const ImVec4& border_col )
     {
         if ( !image )
             return;
 
-        auto it = g_TextureCache.find( image );
-        if ( it != g_TextureCache.end() )
-        {
-            ::ImGui::Image( it->second, size, uv0, uv1, tint_col, border_col );
-            return;
-        }
-
         if ( Graphic::RendererAPI::GetAPIType() == Graphic::RendererAPIType::Vulkan )
         {
+            static std::unordered_map<VkImageView, ImTextureID> g_TextureCache;
+
             const auto vulkanImageInfo =
                  sp_cast<Graphic::API::Vulkan::VulkanImage2D>( image )->GetVulkanImageInfo();
             if ( !vulkanImageInfo.ImageView )
                 return;
 
+            auto it = g_TextureCache.find( vulkanImageInfo.ImageView );
+            if ( it != g_TextureCache.end() )
+            {
+                ::ImGui::Image( it->second, size, uv0, uv1, tint_col, border_col );
+                return;
+            }
+
             ImTextureID textureID = ImGui_ImplVulkan_AddTexture(
                  vulkanImageInfo.Sampler, vulkanImageInfo.ImageView, vulkanImageInfo.Layout );
 
-            g_TextureCache[image] = textureID;
+            g_TextureCache[vulkanImageInfo.ImageView] = textureID;
             ::ImGui::Image( textureID, size, uv0, uv1, tint_col, border_col );
         }
     }
